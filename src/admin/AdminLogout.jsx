d27c4466113788d23cd0297e9ebbc15764db3ca4
Sufiyan <2500031350@kluniversity.in>
2026-02-23 12:28:03 +0530

    project v1.0

---
+++ src/admin/AdminLogout.jsx
@@ -0,0 +1,10 @@
+    import React from 'react';
+    
+    export default function AdminLogout() {
+      return (
+        <div>
+          <h2>Admin Logout</h2>
+        </div>
+      );
+    }
+    
