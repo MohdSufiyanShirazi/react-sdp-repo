97ce65966860f32742e6cc526cc0d0b1f15a5147
Sufiyan <2500031350@kluniversity.in>
2026-03-10 12:00:58 +0530

    project added and modified

--- src/pages/MainNavBar.jsx
+++ src/pages/MainNavBar.jsx
@@ -15,7 +15,7 @@ const MainNavBar = () => {
       <div className="navbar-container">
         <div className="navbar-logo">
           <Link to="/" className="logo-link">
-              Event Manager System
+              Event Manager System - 2500031350
           </Link>
         </div>
         <ul className="nav-menu">
